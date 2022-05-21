890f079376f73d8c0325506403f396b3e90a11e4
Jorgus Borgus <105965859+SatanicNoodle@users.noreply.github.com>
2022-05-21 03:51:32 -0500

    Update bad number game.py

--- bad number game.py
+++ bad number game.py
@@ -1,5 +1,5 @@
-
 import random
+
 from tkinter import Y 
 
 print('Pick A Number From 1 To 10:')
@@ -7,13 +7,7 @@ x = int(input())
 
 y = random.randint(1, 2)
 
-
-
-
 if x == y:
 	print("nice one chuckle nuts, you're correct its %d"%(y))
-
-
 else:
 	print("it was actually %d ya silly goof"%(y))
-
